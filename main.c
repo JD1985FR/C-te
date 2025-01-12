f7803ab3c203d389c0737cbbeb8143331d991804
JD1985FR <jimenez.girard.david@gmail.com>
2025-01-12 01:55:01 +0100

    1.E

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int appleQuantity =23;
+    double applePrice = 1.49;
+    int appleReviewDisplay =82.5;
+    const char appleLocation ='F';
+    // Put all your code above this and if you declare your variables using the given names and types there is no need to change any of the code below.
+    printf("An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%!", applePrice, appleQuantity, appleLocation, appleReviewDisplay);
+
+}
